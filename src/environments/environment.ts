843c39de863034e4f69da1c7ac0df35640a156ea
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro-2.local>
2023-04-19 10:11:00 -0600

    timesheet app done

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+production:false,
+firebase: {
+  apiKey: "AIzaSyCJ_tVA3Xgb-wpMdCdRPgEhRSs311skHbM",
+  authDomain: "timesheetfirebase-fa6bc.firebaseapp.com",
+  projectId: "timesheetfirebase-fa6bc",
+  storageBucket: "timesheetfirebase-fa6bc.appspot.com",
+  messagingSenderId: "570787404764",
+  appId: "1:570787404764:web:d6d5adf99ec18f138c4d19",
+  measurementId: "G-JRW2GTQFBC" 
+}
+
+};
